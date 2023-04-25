da53c1e2b64336f98c712d49c45efe4516a03a00
Paweł <pawel.tylczynski@gmail.com>
2023-04-25 12:08:49 +0200

    docs folder

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 module.exports = {
     entry: { index: path.resolve(__dirname, "source", "index.js") },
     output: {
-        path: path.resolve(__dirname, "build")
+        path: path.resolve(__dirname, "docs")
       },
       module: {
         rules: [
